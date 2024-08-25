14a4a0e8b8cef137b4464feb40f3dde8c2dd019b
Cesar <cesar23augusto16@gmail.com>
2024-08-25 09:26:11 -0600

    prueba de conexión

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const {Client} = require("pg");
 const connectionString ='postgresql://root:F9kwTt3HvqfHkaxxSK3tQMbpq6GGQRdF@dpg-cr56lkdumphs73e1au6g-a.oregon-postgres.render.com/factucot_bd';
+const connection2 = 'postgresql://root:F9kwTt3HvqfHkaxxSK3tQMbpq6GGQRdF@dpg-cr56lkdumphs73e1au6g-a/factucot_bd';
 
 
 const connection = new Client({
-    connectionString,
+    connection2,
     ssl: {
         rejectUnauthorized: false
     }
