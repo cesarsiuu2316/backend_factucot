e3a5812e357f847a52a80920f83ccf3bab7613ca
Cesar <cesar23augusto16@gmail.com>
2024-09-01 01:13:30 -0600

    cambios sin probar de cotizacion y facturas

--- servicios/cotizacion.services.js
+++ servicios/cotizacion.services.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 const getAllCotizaciones = () => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM cotizacion', (err, results) => {
+        db.query('SELECT * FROM "cotizacion"', (err, results) => {
             if (err) {
                 reject(err);
             } else {
@@ -14,13 +14,13 @@ const getAllCotizaciones = () => {
 
 const getCotizacionById = (id) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM cotizacion WHERE id_cotizacion = ?', [id], (err, results) => {
+        db.query('SELECT * FROM "cotizacion" WHERE id_cotizacion = $1', [id], (err, result) => {
             if (err) {
                 reject(err);
-            } else if (results.length === 0) {
+            } else if (result.rows.length === 0) {
                 resolve(null);
             } else {
-                resolve(results[0]);
+                resolve(result.rows[0]);
             }
         });
     });
@@ -30,14 +30,14 @@ const createCotizacion = (cotizacionData) => {
     const { id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente } = cotizacionData;
     return new Promise((resolve, reject) => {
         db.query(
-            `INSERT INTO cotizacion (id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente)
-             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            `INSERT INTO "cotizacion" (id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente)
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id_cotizacion`,
             [id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente],
-            (err, results) => {
+            (err, result) => {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve({ id_cotizacion: results.insertId, ...cotizacionData });
+                    resolve({ id_cotizacion: result.rows[0].id_cotizacion, ...cotizacionData });
                 }
             }
         );
@@ -48,10 +48,10 @@ const updateCotizacion = (id, cotizacionData) => {
     const { id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente } = cotizacionData;
     return new Promise((resolve, reject) => {
         db.query(
-            `UPDATE cotizacion SET id_lista = ?, id_user = ?, fecha_creacion = ?, fecha_caducidad = ?, total = ?, totalNeto = ?, descuento = ?, impuesto = ?, NombreCliente = ?
-             WHERE id_cotizacion = ?`,
+            `UPDATE "cotizacion" SET id_lista = $1, id_user = $2, fecha_creacion = $3, fecha_caducidad = $4, total = $5, totalNeto = $6, descuento = $7, impuesto = $8, NombreCliente = $9
+             WHERE id_cotizacion = $10`,
             [id_lista, id_user, fecha_creacion, fecha_caducidad, total, totalNeto, descuento, impuesto, NombreCliente, id],
-            (err, results) => {
+            (err, result) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -64,7 +64,7 @@ const updateCotizacion = (id, cotizacionData) => {
 
 const deleteCotizacion = (id) => {
     return new Promise((resolve, reject) => {
-        db.query('DELETE FROM cotizacion WHERE id_cotizacion = ?', [id], (err, results) => {
+        db.query('DELETE FROM "cotizacion" WHERE id_cotizacion = $1', [id], (err, results) => {
             if (err) {
                 reject(err);
             } else {
--- servicios/factura.services.js
+++ servicios/factura.services.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 const getAllFacturas = () => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM factura', (err, results) => {
+        db.query('SELECT * FROM "factura"', (err, results) => {
             if (err) {
                 reject(err);
             } else {
@@ -14,13 +14,13 @@ const getAllFacturas = () => {
 
 const getFacturaById = (id) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM factura WHERE id_factura = ?', [id], (err, results) => {
+        db.query('SELECT * FROM "factura" WHERE id_factura = $1', [id], (err, result) => {
             if (err) {
                 reject(err);
-            } else if (results.length === 0) {
+            } else if (result.rows.length === 0) {
                 resolve(null);
             } else {
-                resolve(results[0]);
+                resolve(result.rows[0]);
             }
         });
     });
@@ -30,14 +30,14 @@ const createFactura = (facturaData) => {
     const { fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente } = facturaData;
     return new Promise((resolve, reject) => {
         db.query(
-            `INSERT INTO factura (fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente)
-             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            `INSERT INTO "factura" (fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente)
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id_factura`,
             [fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente],
-            (err, results) => {
+            (err, result) => {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve({ id_factura: results.insertId, ...facturaData });
+                    resolve({ id_factura: result.rows[0].id_factura, ...facturaData });
                 }
             }
         );
@@ -48,8 +48,8 @@ const updateFactura = (id, facturaData) => {
     const { fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente } = facturaData;
     return new Promise((resolve, reject) => {
         db.query(
-            `UPDATE factura SET fecha_creacion = ?, id_user = ?, id_lista = ?, total = ?, totalNeto = ?, descuento = ?, impuesto = ?, RTN = ?, NombreCliente = ?
-             WHERE id_factura = ?`,
+            `UPDATE "factura" SET fecha_creacion = $1, id_user = $2, id_lista = $3, total = $4, totalNeto = $5, descuento = $6, impuesto = $7, RTN = $8, NombreCliente = $9
+             WHERE id_factura = $10`,
             [fecha_creacion, id_user, id_lista, total, totalNeto, descuento, impuesto, RTN, NombreCliente, id],
             (err, results) => {
                 if (err) {
@@ -64,7 +64,7 @@ const updateFactura = (id, facturaData) => {
 
 const deleteFactura = (id) => {
     return new Promise((resolve, reject) => {
-        db.query('DELETE FROM factura WHERE id_factura = ?', [id], (err, results) => {
+        db.query('DELETE FROM "factura" WHERE id_factura = $1', [id], (err, results) => {
             if (err) {
                 reject(err);
             } else {
